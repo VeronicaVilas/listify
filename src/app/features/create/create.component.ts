5297a01da32d22ad5ae2e36cf4de77b82b439cac
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-10-01 11:23:04 -0300

    feat: creates icon options form

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../shared/services/products.service';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule, MatSelectModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  productsService = inject(ProductsService);
+
   form = new FormGroup({
     image: new FormControl<string>("", {
       nonNullable: true,
@@ -31,14 +36,23 @@ export class CreateComponent {
     }),
     status: new FormControl<string>("Não Comprado", {
       nonNullable: true,
-    })
+    }),
+    icon: new FormControl<string>("❓", {
+      nonNullable: true,
+    }),
   })
 
   onSubmit() {
-    this.form.controls.image.value;
-    this.form.controls.title.value;
-    this.form.controls.category.value;
-    this.form.controls.amount.value;
-    this.form.controls.status.value;
+    this.productsService. post({
+      image: this.form.controls.image.value,
+      title: this.form.controls.title.value,
+      category: this.form.controls.category.value,
+      amount: this.form.controls.amount.value,
+      status: this.form.controls.status.value,
+      icon: this.form.controls.icon.value,
+    })
+    .subscribe(() => {
+      alert('Sucesso!');
+    });
   }
 }
