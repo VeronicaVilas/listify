9adc447e958ac8bf506fddc7565a6e9cfdcbd9bb
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-10-01 09:10:30 -0300

    refactor: adds table for product listing

--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -1,10 +1,13 @@
 import { Component, computed, input } from '@angular/core';
 import { Product } from '../../../../shared/interfaces/product.interface';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -12,4 +15,5 @@ export class CardComponent {
   product = input.required<Product>();
 
   productTitle = computed(() => this.product().title);
+  productListName = computed(() => this.product().category);
 }
