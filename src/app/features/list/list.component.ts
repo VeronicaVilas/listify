fe1b2172ccfa37c1aae01be7dc2404a8cd73eeda
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-10-01 14:36:22 -0300

    refactor: refactor products table into listComponent

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,8 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, input } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-import { CardComponent } from './components/card/card.component';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
@@ -10,11 +8,14 @@ import { CreateComponent } from '../create/create.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { CategoryCardsComponent } from './components/category-cards/category-cards.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, CategoryCardsComponent, MatButtonModule, MatDialogModule, MatCardModule, MatIconModule],
+  imports: [RouterLink, CategoryCardsComponent, MatButtonModule, MatDialogModule, MatCardModule, MatIconModule, MatTableModule, MatChipsModule, MatSlideToggleModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
