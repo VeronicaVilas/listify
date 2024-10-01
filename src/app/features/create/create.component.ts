f17f390e9c9d65f5a39a3cd74f933c7d1a61f171
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-10-01 09:08:06 -0300

    feat: adds field to product creation form

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  form = new FormGroup({
+    image: new FormControl<string>("", {
+      nonNullable: true,
+    }),
+    title: new FormControl<string>("", {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    category: new FormControl<string>("", {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    amount: new FormControl<number>(0, {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    status: new FormControl<string>("Não Comprado", {
+      nonNullable: true,
+    })
+  })
 
+  onSubmit() {
+    this.form.controls.image.value;
+    this.form.controls.title.value;
+    this.form.controls.category.value;
+    this.form.controls.amount.value;
+    this.form.controls.status.value;
+  }
 }
