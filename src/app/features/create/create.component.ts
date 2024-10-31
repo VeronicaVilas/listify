e2a839299387fa64002629135aab5231548ed924
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-10-31 12:07:52 -0300

    refactor: adds userId to product creation

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,14 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, Inject, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductsService } from '../../shared/services/products.service';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { merge } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-create',
@@ -20,8 +18,9 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 export class CreateComponent {
 
   productsService = inject(ProductsService);
-
   matSnackBar = inject(MatSnackBar)
+
+  userId: string;
 
   form = new FormGroup({
     image: new FormControl<string>("", {
@@ -44,6 +43,11 @@ export class CreateComponent {
     }),
   });
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    // Capturando o userId dos dados passados
+    this.userId = data.userId;
+  }
+
   onSubmit() {
     if (this.form.valid) {
       this.productsService.post({
@@ -51,7 +55,8 @@ export class CreateComponent {
         title: this.form.controls.title.value,
         category: this.form.controls.category.value,
         amount: this.form.controls.amount.value,
-        included: this.form.controls.included.value
+        included: this.form.controls.included.value,
+        userId: this.userId
       }).subscribe(() => {
         this.matSnackBar.open('Produto criado com sucesso!', 'Ok', {
           duration: 3000,
